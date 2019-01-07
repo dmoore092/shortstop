1347701718222d635517606f403f14c53ace8635
Dale Moore <dmoore092@gmail.com>
2019-01-07 17:59:47 -0500

    ..

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -4,7 +4,7 @@
         <title><?php echo $title ?> | Athletic Prospects</title>
       <!--  <link rel="stylesheet" type="text/css" href="assets/css/main.css" /> -->
         <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
-        <link rel="stylesheet" type="text/css" href="assets/css/<?php echo $page ?>.css" />
+        <link rel="stylesheet" type="text/css" href="/assets/css/<?php echo $page ?>.css" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"><!--icon library -->
         <meta http-equiv="content-type" content="text/php; charset=utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">

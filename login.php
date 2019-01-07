353a519b5fcbfc43856a7c136e250f42e96117ba
Dale Moore <dmoore092@gmail.com>
2019-01-07 17:59:00 -0500

    ..

--- login.php
+++ login.php
@@ -8,7 +8,7 @@
       include "classes/Player.PDO.class.php";
       $_SESSION['logged_in'] = 'false';
     if(isset($_POST["login"])){
-        echo "login called";
+        //echo "login called";
         $username = htmlentities(strip_tags(trim($_POST["username"])));//$_POST["username"];
         $password = htmlentities(strip_tags(trim($_POST["password"])));//$_POST["password"];
 
@@ -17,10 +17,10 @@
         $isLoggedIn = $player->login($username, $password);
     }
     if(isset($_SESSION['loggedIn']) && $_SESSION['loggedIn']) {
-        echo "<h1>Logged in</h1>";
+        //echo "<h1>Logged in</h1>";
         //var_dump($_SESSION['id']);
         //$id = $_SESSION['id'];
-        header("Location: profile.php?id={$_SESSION['id']}");
+        header("Location: /profile.php?id={$_SESSION['id']}");
       }
 ?>
 <?php include('assets/inc/header.inc.php'); ?>

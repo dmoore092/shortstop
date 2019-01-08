5123d6450c752997b83cf8615233b7c8559af61a
Dale Moore <dmoore092@gmail.com>
2019-01-07 20:48:12 -0500

    ..

--- assets/inc/search.inc.php
+++ assets/inc/search.inc.php
@@ -1,5 +1,5 @@
 <?php
-    include_once ("/var/www//html/classes/Player.PDO.class.php");
+    include_once ("/var/www/html/classes/Player.PDO.class.php");
     $playerDB = new PlayerDB();
 
     $data = null;

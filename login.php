dcb5e0a563874200302371e3533900daae72eaac
Dale Moore <dmoore092@gmail.com>
2019-01-08 15:44:34 -0500

    ..

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
       $linkpath = "";
       $templinkpath = "";
       
-      include("/var/www/html/classes/Player.PDO.class.php");
+      include("/var/www/html/classes/Player.PDO.Class.php");
       $_SESSION['logged_in'] = 'false';
     if(isset($_POST["login"])){
         //echo "login called";

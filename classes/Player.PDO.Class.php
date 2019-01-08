9454f334d565930ffea5168f9fdf2105beddd77d
Dale Moore <dmoore092@gmail.com>
2019-01-08 06:59:32 -0500

    ..

--- classes/Player.PDO.Class.php
+++ classes/Player.PDO.Class.php
@@ -1,7 +1,7 @@
 <?php 
-    require_once ("classes/PDO.DB.class.php");
+    require_once ("/var/www/html/classes/classes/PDO.DB.class.php");
 
-	include("classes/Player.class.php");
+	include("/var/www/html/classes/classes/Player.class.php");
 
 	/*
 	* UserDB class contains all of the methods for using PHP Data Objects to 

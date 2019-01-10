af608006b9058115f1596dcc5bb9f4f1b21d950d
Dale Moore <dmoore092@gmail.com>
2019-01-10 17:11:48 -0500

    .

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -1,11 +1,11 @@
 <footer>
     <div id="foot-wrapper">
-        <p>&copy; 2018 Athletic Prospects</p>
         <div class="social-media" id="sm-footer">
             <a href="http://www.facebook.com" target="_blank" class="fa fa-facebook"></a>
             <a href="http://www.twitter.com" target="_blank" class="fa fa-twitter"</a>
             <a href="http://www.instagram.com" target="_blank" class="fa fa-instagram"></a>
-        </div> <!-- end of social-media
+        </div> <!-- end of social-media -->
+        <p>&copy; 2018 Athletic Prospects</p>
     </div> <!-- end of foot-wrapper -->
 </footer>
             </div><!-- end of #body-main -->

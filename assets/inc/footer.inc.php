84d401bff7b5fb972d3bd99c9982dee8fe9c545b
Dale Moore <dmoore092@gmail.com>
2019-01-10 17:22:02 -0500

    .

--- assets/inc/footer.inc.php
+++ assets/inc/footer.inc.php
@@ -5,7 +5,7 @@
             <a href="http://www.twitter.com" target="_blank" class="fa fa-twitter"</a>
             <a href="http://www.instagram.com" target="_blank" class="fa fa-instagram"></a>
         </div> <!-- end of social-media -->
-        <p>&copy; 2018 Athletic Prospects</p>
+        <p id="copyright">&copy; 2018 Athletic Prospects</p>
     </div> <!-- end of foot-wrapper -->
 </footer>
             </div><!-- end of #body-main -->

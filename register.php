40cbbc721b1a37a15cf7ce84d9524984b504308a
Dale Moore <dmoore092@gmail.com>
2019-06-04 14:42:44 -0400

    turning off captchas until i get domain name

--- register.php
+++ register.php
@@ -64,17 +64,17 @@
                 <?php
                 
                     if(isset($_POST["btnCreate"])){
-                        if(isset($_POST['g-recaptcha-response'])){
-                            $captcha=$_POST['g-recaptcha-response'];
-                            if(!$captcha){
-                                echo "<p style='color:red';>Please check the ReCaptcha!</p>";
-                                exit;
-                            }
-                            $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfGGJEUAAAAAFAw4zjaPVM2rlP1HqtQBw05rCek&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
-                            if($response['success'] == false){
-                                echo "<p style='color:red';>ReCaptcha Failed.</p>";
-                            }
-                            else{
+                        // if(isset($_POST['g-recaptcha-response'])){
+                        //     $captcha=$_POST['g-recaptcha-response'];
+                        //     if(!$captcha){
+                        //         echo "<p style='color:red';>Please check the ReCaptcha!</p>";
+                        //         exit;
+                        //     }
+                        //     $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfGGJEUAAAAAFAw4zjaPVM2rlP1HqtQBw05rCek&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+                        //     if($response['success'] == false){
+                        //         echo "<p style='color:red';>ReCaptcha Failed.</p>";
+                        //     }
+                        //     else{
                                 //block cross-site scripting, html entities(apersand etc), trim white space
                                 $username  = htmlentities(strip_tags(trim($_POST["username"])));
                                 $password  = htmlentities(strip_tags(trim($_POST["retypepassword"])));
@@ -102,8 +102,8 @@
                                 else{
                                     echo "Passwords do not match";
                                 }
-                            }
-                        }
+                            //}
+                        //}
                     }
                 ?>
             </form>      

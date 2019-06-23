db57b30d47e816e35ad6a3903dac904b32029e7f
Dale Moore <dmoore092@gmail.com>
2019-06-23 08:25:14 -0400

    making recaptchas work for live

--- login.php
+++ login.php
@@ -70,19 +70,19 @@
     //reset the users password. From changepassword.php
     if(isset($_POST['update-password'])){
         //makes recaptcha work
-        // if(isset($_POST['g-recaptcha-response'])){
-        //     $captcha=$_POST['g-recaptcha-response'];
-        //     //captcha failed
-        //     if(!$captcha){
-        //         echo "<p style='color:red';>Please check the ReCaptcha!</p>";
-        //         exit;
-        //     }
-        //     $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfGGJEUAAAAAFAw4zjaPVM2rlP1HqtQBw05rCek&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
-        //     if($response['success'] == false){
-        //         echo "<p style='color:red';>ReCaptcha Failed.</p>";
-        //     }
-        //     //captcha passed
-        //     else{
+        if(isset($_POST['g-recaptcha-response'])){
+            $captcha=$_POST['g-recaptcha-response'];
+            //captcha failed
+            if(!$captcha){
+                echo "<p style='color:red';>Please check the ReCaptcha!</p>";
+                exit;
+            }
+            $response=json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=6LfGGJEUAAAAAFAw4zjaPVM2rlP1HqtQBw05rCek&response=".$captcha."&remoteip=".$_SERVER['REMOTE_ADDR']), true);
+            if($response['success'] == false){
+                echo "<p style='color:red';>ReCaptcha Failed.</p>";
+            }
+            //captcha passed
+            else{
                 //check the user typed the same password both times
                 if($_POST["password"] == $_POST['retypepassword']){
                     if($_POST['password'] != ""){
@@ -103,8 +103,8 @@
                 else{
                     echo "<p style='color:red;margin-top:25px'>Passwords do not match, try again</p>";
                 }
-            //}
-        //}  
+            }
+        }  
     }
 
 

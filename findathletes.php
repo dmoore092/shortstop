b987037442008dd1bce251798683020f1d94bbcd
Dale Moore <dmoore092@gmail.com>
2019-06-23 12:32:24 -0400

    troublshooting search by class

--- findathletes.php
+++ findathletes.php
@@ -107,11 +107,11 @@
         </select>
         <select name='class'>
             <option value=' ' selected disabled>Class of:</option>
-            <option value='2019'>2024</option>
-            <option value='2018'>2023</option>
-            <option value='2017'>2022</option>
-            <option value='2016'>2021</option>
-            <option value='2015'>2020</option>
+            <option value='2024'>2024</option>
+            <option value='2023'>2023</option>
+            <option value='2022'>2022</option>
+            <option value='2021'>2021</option>
+            <option value='2020'>2020</option>
             <option value='2019'>2019</option>
             <option value='2018'>2018</option>
             <option value='2017'>2017</option>

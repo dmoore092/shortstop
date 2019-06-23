0b4d27817be3a38dc5b704d635c5cb5713fd57d7
Dale Moore <dmoore092@gmail.com>
2019-06-23 12:34:12 -0400

    fixed search by class

--- classes/Player.PDO.Class.php
+++ classes/Player.PDO.Class.php
@@ -795,6 +795,11 @@
 						</select>
 						<select name='class'>
 							<option value=' ' selected disabled>Class of:</option>
+							<option value='2024'>2024</option>
+							<option value='2023'>2023</option>
+							<option value='2022'>2022</option>
+							<option value='2021'>2021</option>
+							<option value='2022'>2020</option>
 							<option value='2019'>2019</option>
 							<option value='2018'>2018</option>
 							<option value='2017'>2017</option>

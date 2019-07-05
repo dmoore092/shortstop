47b2e33cc0320bc7f1003ddc72d7ba0bebd472ca
Dale Moore <dmoore092@gmail.com>
2019-07-05 00:32:49 -0400

    working on stripe

--- profile.php
+++ profile.php
@@ -12,34 +12,7 @@
 <?php include('assets/inc/header.inc.php'); ?>  
 
 <script src="https://js.stripe.com/v3"></script>
-<script>
-  var stripe = Stripe('pk_live_S2WeKKv4ANIOBSjI3FdXx5Uf00TTNsDx2j');
 
-  var checkoutButton = document.getElementById('checkout-button-plan_FJ7HouBZeAK4zB');
-  checkoutButton.addEventListener('click', function () {
-    // When the customer clicks on the button, redirect
-    // them to Checkout.
-    stripe.redirectToCheckout({
-      items: [{plan: 'plan_FJ7HouBZeAK4zB', quantity: 1}],
-
-      // Do not rely on the redirect to the successUrl for fulfilling
-      // purchases, customers may not always reach the success_url after
-      // a successful payment.
-      // Instead use one of the strategies described in
-      // https://stripe.com/docs/payments/checkout/fulfillment
-      successUrl: window.location.protocol + '//www.athleticprospects.com/profile.php?id=2',
-      cancelUrl: window.location.protocol + '//www.athleticprospects.com/profile.php?id=2',
-    })
-    .then(function (result) {
-      if (result.error) {
-        // If `redirectToCheckout` fails due to a browser or network
-        // error, display the localized error message to your customer.
-        var displayError = document.getElementById('error-message');
-        displayError.textContent = result.error.message;
-      }
-    });
-  });
-</script>
 
 <!-- <script src="https://js.stripe.com/v3/"></script>
 <script>
@@ -477,4 +450,32 @@
 							<p>Restricted</p>
 					</div>
 <?php endif; ?>
+<script>
+  var stripe = Stripe('pk_live_S2WeKKv4ANIOBSjI3FdXx5Uf00TTNsDx2j');
+
+  var checkoutButton = document.getElementById('checkout-button-plan_FJ7HouBZeAK4zB');
+  checkoutButton.addEventListener('click', function () {
+    // When the customer clicks on the button, redirect
+    // them to Checkout.
+    stripe.redirectToCheckout({
+      items: [{plan: 'plan_FJ7HouBZeAK4zB', quantity: 1}],
+
+      // Do not rely on the redirect to the successUrl for fulfilling
+      // purchases, customers may not always reach the success_url after
+      // a successful payment.
+      // Instead use one of the strategies described in
+      // https://stripe.com/docs/payments/checkout/fulfillment
+      successUrl: window.location.protocol + '//www.athleticprospects.com/profile.php?id=2',
+      cancelUrl: window.location.protocol + '//www.athleticprospects.com/profile.php?id=2',
+    })
+    .then(function (result) {
+      if (result.error) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer.
+        var displayError = document.getElementById('error-message');
+        displayError.textContent = result.error.message;
+      }
+    });
+  });
+</script>
 <?php include("assets/inc/footer.inc.php"); ?>

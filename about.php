cdd370699b4e0254ce13a823125a2d2c52a5ff34
Dale Moore <dmoore092@gmail.com>
2019-07-07 11:46:38 -0400

    stripping tags from site content pulled from db

--- about.php
+++ about.php
@@ -10,8 +10,8 @@
                     $query = "SELECT header, text FROM about_us ORDER BY id DESC LIMIT 1;";
                     $result = mysqli_query($conn, $query);
                     while($row = mysqli_fetch_assoc($result)){
-                        strip_tags($row['header']);
-                        strip_tags($row['text']);
+                        strip_tags(html_entity_decode($row['header']));
+                        strip_tags(html_entity_decode($row['text']));
                         echo "<h2>{$row['header']}</h2>";
                         echo "<p>{$row['text']}</p>";
                     }

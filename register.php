903b8ba9317aa6e0ee80a8bf6511e151bb8541c9
Dale Moore <dmoore092@gmail.com>
2019-08-03 14:37:09 -0400

    supressing any error messages

--- register.php
+++ register.php
@@ -1,4 +1,4 @@
-<?php include("config/pageconfig.php"); session_start(); error_reporting(E_ALL); ?>
+<?php include("config/pageconfig.php"); session_start(); error_reporting(0); ?>
 <?php include_once ("classes/Player.PDO.Class.php"); ?>
 <?php include("assets/inc/header.inc.php");?>
 

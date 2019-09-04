b71122f4299c9761c77beb4ab05f3404c5247ade
Dale Moore <dmoore092@gmail.com>
2019-09-04 10:18:09 -0400

    testing, and re-enabled keiths address for registration emails

--- assets/inc/create_account.php
+++ assets/inc/create_account.php
@@ -41,7 +41,7 @@ require_once './PHPMailer/src/SMTP.php';
                         //Recipients
                         $mail->setFrom('webmaster@athleticprospects.com', 'Athletic Prospects');
                         $mail->addAddress('dmoore092@gmail.com', 'Dale');     // Add a recipient
-                        //$mail->addAddress('kprestano@athleticprospects.com', 'Keith'); // Name is optional
+                        $mail->addAddress('kprestano@athleticprospects.com', 'Keith'); // Name is optional
                         
                         //$mail->addReplyTo('info@example.com', 'Information');
                         //$mail->addCC('cc@example.com');

bf4964a269ac2e91231fc78d715b0315d702e7fa
Dale Moore <dmoore092@gmail.com>
2019-09-06 11:42:44 -0400

    google analytics testing

--- assets/inc/header.inc.php
+++ assets/inc/header.inc.php
@@ -1,6 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-141642555-3"></script>
+        <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-141642555-3');
+        </script>
+
+
         <title><?php echo $TITLE ?> | Athletic Prospects</title>
         <link rel="stylesheet" type="text/css" href="assets/css/main.css" />
         <link rel="stylesheet" type="text/css" href="assets/css/footer.css" />

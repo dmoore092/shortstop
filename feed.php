61998c00edee4706ae372c915d80a714592f0b7f
Dale Moore <dmoore092@gmail.com>
2019-12-17 15:34:21 -0500

    fixing itunes tags in feed

--- feed.php
+++ feed.php
@@ -7,14 +7,14 @@
     $rssfeed .= '<title>Change This Title</title>';
     $rssfeed .= '<link>https://www.athleticprospects.com</link>';
     $rssfeed .= '<description>Athletic Prospects RSS feed</description>';
+    $rssfeed .= "<itunes:owner>";
+    $rssfeed .= "<itunes:name>keith Prestano</itunes:name>";
+    $rssfeed .= "<itunes:email>kprestano@athleticprospects.com</itunes:email>";
+    $rssfeed .= "</itunes:owner>";
+    $rssfeed .= "<itunes:image href='/assets/img/black.JPG' />";
+    $rssfeed .= "<itunes:category text='sports'/>";
     $rssfeed .= '<language>en-us</language>';
     $rssfeed .= '<copyright>Copyright (C) 2019 athleticprospects.com</copyright>';
-    $rssfeed .= "<itunes:owner>";
-    //$rssfeed .= "<itunes:name>keith Prestano</itunes:name>";
-    //$rssfeed .= "<itunes:email>kprestano@athleticprospects.com</itunes:email>";
-    $rssfeed .= "</itunes:owner>";
-    //$rssfeed .= "<itunes:image href='/assets/img/black.JPG' />";
-    //$rssfeed .= "<itunes:category text='sports'/>";
  
     $connection = mysqli_connect('127.0.0.1', 'root', 'y#GbqXtBGcy!z3Cf', 'sports')
         or die('Could not connect to database');
@@ -30,7 +30,7 @@
         $rssfeed .= '<description>' . $description . '</description>';
         $rssfeed .= "<link>https://www.athleticprospects.com</link>";
         $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($post_date)) . '</pubDate>';
-        //$rssfeed .= "<itunes:explicit>no</itunes:explicit>";
+        $rssfeed .= "<itunes:explicit>no</itunes:explicit>";
         $rssfeed .= "<enclosure url='https://www.athleticprospects.com/assets/audio/" . $podcast . ".mp3' length='". $filesize ."' type='audio/mpeg' />";
         $rssfeed .= "<guid>https://www.athleticprospects.com/assets/audio/" . $podcast . ".mp3</guid>";
         $rssfeed .= '</item>';

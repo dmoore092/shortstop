ff5ac3fd2b4e92165b0ca038ba2c3536eee4c31e
Dale Moore <dmoore092@gmail.com>
2019-12-17 16:01:44 -0500

    fixing itunes tags in feed

--- feed.php
+++ feed.php
@@ -2,20 +2,22 @@
     header("Content-Type: application/rss+xml; charset=ISO-8859-1");
  
     $rssfeed = '<?xml version="1.0" encoding="ISO-8859-1"?>';
-    $rssfeed .= '<rss version="2.0">';
+    $rssfeed .= '<rss version="2.0" xmlns:itunes="http://www.itunes.com/dtds/podcast-1.0.dtd" xmlns:content="http://purl.org/rss/1.0/modules/content/">';
     $rssfeed .= '<channel>';
-    $rssfeed .= '<title>Change This Title</title>';
+    $rssfeed .= '<title>Athletic Prospects</title>';
     $rssfeed .= '<link>https://www.athleticprospects.com</link>';
-    $rssfeed .= "<author>Athletic Prospects</author>";
+    $rssfeed .= "<itunes:author>Athletic Prospects</itunes:author>";
     $rssfeed .= '<description>Athletic Prospects RSS feed</description>';
+    $rssfeed .= "<itunes:type>serial</itunes:type>";
     $rssfeed .= "<itunes:owner>";
-    $rssfeed .= "<itunes:name>keith Prestano</itunes:name>";
+    $rssfeed .= "<itunes:name>Athletic Prospects</itunes:name>";
     $rssfeed .= "<itunes:email>kprestano@athleticprospects.com</itunes:email>";
     $rssfeed .= "</itunes:owner>";
     $rssfeed .= "<itunes:image href='/assets/img/podcastImage.jpg' />";
-    $rssfeed .= "<itunes:category text='sports'/>";
-    $rssfeed .= '<language>en-us</language>';
-    $rssfeed .= '<copyright>Copyright (C) 2019 athleticprospects.com</copyright>';
+    $rssfeed .= "<itunes:category text='Sports'><itunes:category text='Baseball'/></itunes:category>";
+    $rssfeed .= "<itunes:explicit>false</itunes:explicit>";
+    //$rssfeed .= '<language>en-us</language>';
+    //$rssfeed .= '<copyright>Copyright (C) 2019 athleticprospects.com</copyright>';
  
     $connection = mysqli_connect('127.0.0.1', 'root', 'y#GbqXtBGcy!z3Cf', 'sports')
         or die('Could not connect to database');
@@ -27,12 +29,12 @@
         extract($row);
 
         $rssfeed .= '<item>';
-        $rssfeed .= '<title>' . $title . '</title>';
+        $rssfeed .= '<itunes:title>' . $title . '</itunes:title>';
         $rssfeed .= "<author>Keith Prestano</author>";
         $rssfeed .= '<description>' . $description . '</description>';
         $rssfeed .= "<link>https://www.athleticprospects.com</link>";
         $rssfeed .= '<pubDate>' . date("D, d M Y H:i:s O", strtotime($post_date)) . '</pubDate>';
-        $rssfeed .= "<itunes:explicit>no</itunes:explicit>";
+        $rssfeed .= "<itunes:explicit>false</itunes:explicit>";
         $rssfeed .= "<enclosure url='https://www.athleticprospects.com/assets/audio/" . $podcast . ".mp3' length='". $filesize ."' type='audio/mpeg' />";
         $rssfeed .= "<guid>https://www.athleticprospects.com/assets/audio/" . $podcast . ".mp3</guid>";
         $rssfeed .= '</item>';
